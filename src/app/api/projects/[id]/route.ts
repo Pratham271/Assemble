200222a672ce4d57a1694a81aac6d925bbdbebe5
Pratham271 <pratham19csu221@ncuindia.edu>
2025-01-10 11:36:19 +0530

    updated the deleted project route

--- src/app/api/projects/[id]/route.ts
+++ src/app/api/projects/[id]/route.ts
@@ -4,8 +4,14 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function DELETE(request: Request, { params }: { params: { id: string } }) {
-    const task = await prisma.project.delete({
+
+    const deletedTasks = await prisma.task.deleteMany({
+      where: {
+        projectId: params.id
+      }
+    })
+    const deletedProject = await prisma.project.delete({
       where: { id: params.id },
     });
-    return NextResponse.json(task);
+    return NextResponse.json(deletedProject);
   }
